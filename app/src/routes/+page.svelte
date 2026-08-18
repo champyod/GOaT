55d6a69d5c12d3ebd22189d0a7155d6904e8b265
KunanonMWIT <kunanon.kha_g34@mwit.ac.th>
2026-08-18 15:29:11 +0700

    Feat: Fix two-way communication issues.

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import { invoke } from '@tauri-apps/api/core';
 
-  let response = "Waiting for ping...";
+  let response = $state("Waiting for ping...");
 
   async function doPing() {
     response = await invoke('ping');
@@ -10,6 +10,6 @@
 
 <main>
   <h1>GOaT IPC Test</h1>
-  <button on:click={doPing}>Send Ping to Rust</button>
+  <button onclick={doPing}>Send Ping to Rust</button>
   <p>Result: {response}</p>
 </main>
